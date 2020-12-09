f8028641e20ba0a7c6f9b5bc9dcc39dd4c294d21
okafor3 <71651742+okafor3@users.noreply.github.com>
2020-12-09 17:01:28 -0600

    updates

--- Main.js
+++ Main.js
@@ -1,11 +1,3 @@
-// // UIkit CSS
-// <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.5.9/dist/css/uikit.min.css" />
-
-// // UIkit JS
-// <script src="https://cdn.jsdelivr.net/npm/uikit@3.5.9/dist/js/uikit.min.js"></script>
-// <script src="https://cdn.jsdelivr.net/npm/uikit@3.5.9/dist/js/uikit-icons.min.js"></script>
-
-
 var togglestate = "normal"
 
 function toggleButton(togglebutton) {
